5c00ec5080460cd5786ec17de87268464eee988a
Ninie0424 <116635360+Ninie0424@users.noreply.github.com>
2024-04-17 15:21:53 +0800

    Add files via upload
    改善手機板跑板問題

--- javascript/main.js
+++ javascript/main.js
@@ -2,20 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     //手機版選單
     let menuBtn  = document.querySelector('.menuBtn');
     let menuBox  = document.querySelector('.menuBox');
-    let navlist   = document.querySelector('nav');
     menuBtn.addEventListener('click', function() {
         if( !menuBtn.classList.contains('open') ) {
             menuBtn.classList.add('open');
             menuBox.classList.add('show');
-            if( !navlist.classList.contains('sticky') && window.pageYOffset < navlist.offsetHeight ) {
-                navlist.classList.add('sticky');
-            }
         } else {
             menuBtn.classList.remove('open');
             menuBox.classList.remove('show');
-            if( navlist.classList.contains('sticky') && window.pageYOffset < navlist.offsetHeight ) {
-                navlist.classList.remove('sticky');
-            }
         }
     });
 
@@ -28,6 +21,8 @@ var toPortfolio = document.querySelector("#portfolio");
 var block2 = document.querySelector(".block2");
 var toOther = document.querySelector("#other");
 var block3 = document.querySelector(".block3");
+let menuBtn  = document.querySelector('.menuBtn');
+let menuBox  = document.querySelector('.menuBox');
 
 function to(toEl) {
     let bridge = toEl;
@@ -48,12 +43,18 @@ function to(toEl) {
 
 toAbout.addEventListener("click",function(){
     to(block1)
+    menuBox.classList.remove('show');
+    menuBtn.classList.remove('open');
 });
 
 toPortfolio.addEventListener("click",function(){
     to(block2)
+    menuBox.classList.remove('show');
+    menuBtn.classList.remove('open');
 });
 
 toOther.addEventListener("click",function(){
     to(block3)
+    menuBox.classList.remove('show');
+    menuBtn.classList.remove('open');
 });
